8617586d496dd14248b146f4694743811f735740
Hyeonseok Jang <jang.hyeonseok@linkbaljang.local>
2023-08-25 21:23:58 +0900

    ADD mechanism of socketio

--- App.js
+++ App.js
@@ -4,28 +4,30 @@ const { Server } = require('socket.io');
 const server = require('http').createServer(app)
 require('dotenv').config()
 // process.env.FRONTEND_PORT...
+const FRONTEND_URL = `${process.env.FRONTEND_HOST}:${process.env.FRONTEND_PORT}`
 const io = new Server(server, {
   cors: {
-    origin: `${FRONTEND_HOST}:${FRONTEND_POST}`,
+    origin: "http://localhost:7070",
     methods: ['GET', 'POST'],
     allowedHeaders: ["my-custom-header"],
     credentials: true,
   }
 })
 
-const USERS = {}
+const chatRoom = {}
 
-const socketRoom = {}
+const userJoinedRoom = {}
 
 const MAX = 2
 
 io.on('connection', (socket) => {
-  console.log(socket)
+  console.log('connection:', socket.id)
   socket.on("join_room", (data) => {
+    console.log('join_room:', socket.id, data)
     // 방이 기존에 생성되어 있다면
-    if (USERS[data.room]) {
+    if (chatRoom[data.room]) {
       // 현재 입장하려는 방에 있는 인원수
-      const currentRoomLength = USERS[data.room].length;
+      const currentRoomLength = chatRoom[data.room].length;
       if (currentRoomLength === MAX) {
         // 인원수가 꽉 찼다면 돌아갑니다.
         socket.to(socket.id).emit("room_full");
@@ -33,59 +35,64 @@ io.on('connection', (socket) => {
       }
 
       // 여분의 자리가 있다면 해당 방 배열에 추가해줍니다.
-      USERS[data.room] = [...USERS[data.room], { id: socket.id }];
+      chatRoom[data.room] = [...chatRoom[data.room], { id: socket.id }];
     } else {
       // 방이 존재하지 않다면 값을 생성하고 추가해줍시다.
-      USERS[data.room] = [{ id: socket.id }];
+      chatRoom[data.room] = [{ id: socket.id }];
     }
-    socketRoom[socket.id] = data.room;
+    userJoinedRoom[socket.id] = data.room;
 
     // 입장
     socket.join(data.room);
 
     // 입장하기 전 해당 방의 다른 유저들이 있는지 확인하고
     // 다른 유저가 있었다면 offer-answer을 위해 알려줍니다.
-    const others = USERS[data.room].filter((user) => user.id !== socket.id);
+    const others = chatRoom[data.room].filter((user) => user.id !== socket.id);
     if (others.length) {
-      io.sockets.to(socket.id).emit("all_USERS", others);
+      io.sockets.to(socket.id).emit("all_users", others);
     }
   });
 
-  socket.on("offer", (sdp, roomName) => {
+  socket.on("offer", (sdp, roomID) => {
+    console.log('offer', socket.id)
     // offer를 전달받고 다른 유저들에게 전달해 줍니다.
-    socket.to(roomName).emit("getOffer", sdp);
+    socket.to(roomID).emit("getOffer", sdp);
   });
 
-  socket.on("answer", (sdp, roomName) => {
+  socket.on("answer", (sdp, roomID) => {
+    console.log('answer', socket.id, roomID)
     // answer를 전달받고 방의 다른 유저들에게 전달해 줍니다.
-    socket.to(roomName).emit("getAnswer", sdp);
+    socket.to(roomID).emit("getAnswer", sdp);
   });
 
-  socket.on("candidate", (candidate, roomName) => {
+  socket.on("candidate", (candidate, roomID) => {
+    console.log('candidate:', socket.id, roomID);
     // candidate를 전달받고 방의 다른 유저들에게 전달해 줍니다.
-    socket.to(roomName).emit("getCandidate", candidate);
+    socket.to(roomID).emit("getCandidate", candidate);
   });
 
   socket.on("disconnect", () => {
+    console.log('disconnect:', socket.id);
     // 방을 나가게 된다면 socketRoom과 USERS의 정보에서 해당 유저를 지워줍니다.
-    const roomID = socketRoom[socket.id];
+    const roomID = userJoinedRoom[socket.id];
 
-    if (USERS[roomID]) {
-      USERS[roomID] = USERS[roomID].filter((user) => user.id !== socket.id);
-      if (USERS[roomID].length === 0) {
-        delete USERS[roomID];
+    if (chatRoom[roomID]) {
+      chatRoom[roomID] = chatRoom[roomID].filter((user) => user.id !== socket.id);
+      if (chatRoom[roomID].length === 0) {
+        delete chatRoom[roomID];
         return;
       }
     }
-    delete socketRoom[socket.id];
-    socket.broadcast.to(USERS[roomID]).emit("user_exit", { id: socket.id });
+    delete userJoinedRoom[socket.id];
+    socket.broadcast.to(chatRoom[roomID]).emit("user_exit", { id: socket.id });
   });
 })
 
+app.get('/', (req, res) => {
+  console.log('get /');
+  res.send('hello, world!')
+})
 
-app.listen(process.env.PORT || 7014, () => {
-  console.log('Server is running')
-  app.get('/', (req, res) => {
-    res.send('hello, world!');
-  })
+server.listen(process.env.PORT || 7014, () => {
+  console.log(`Server is running in ${process.env.PORT}`)
 })
